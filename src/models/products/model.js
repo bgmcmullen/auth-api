6da1c404116c8ffb1dd997dd4ae4d02b7943b013
Brendan <bgmcmullen@comcast.net>
2024-06-22 23:43:05 -0400

    updated products

--- src/models/products/model.js
+++ src/models/products/model.js
@@ -3,6 +3,7 @@
 const productModel = (sequelize, DataTypes) => sequelize.define('Products', {
   name: { type: DataTypes.STRING, required: true },
   description: { type: DataTypes.STRING, required: false },
+  info: { type: DataTypes.TEXT, required: false },
   image: { type: DataTypes.STRING, required: false },
   category: { type: DataTypes.STRING, required: true },
   price: { type: DataTypes.FLOAT, required: true },
